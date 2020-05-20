0478b17734a31b14a8897be5118532999eb66f36
Engin Demirbilek <engindemirbilek@protonmail.com>
2020-05-20 12:44:21 +0300

    Front-End encs

--- getin.php
+++ getin.php
@@ -14,7 +14,7 @@ include 'functions/login.function.php';
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>The NorthStar Login</title>
+  <title>NorthStar Login</title>
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="css/ruang-admin.min.css" rel="stylesheet">
